672dc258a08d86ad8992216aff2d493de64f4b7b
Aaron (Banana) Peddle <aaron@banana.dev>
2023-11-11 05:10:46 +0000

    bananas

--- app.py
+++ app.py
@@ -1,4 +1,8 @@
 from potassium import Potassium, Request, Response
+from transformers.utils.import_utils import subprocess
+
+from audiocraft.models.musicgen import MusicGen
+from audiocraft.data.audio import audio_write
 
 from transformers import pipeline
 import torch
@@ -9,8 +13,10 @@ app = Potassium("my_app")
 @app.init
 def init():
     device = 0 if torch.cuda.is_available() else -1
-    model = pipeline('fill-mask', model='bert-base-uncased', device=device)
-   
+    model = MusicGen.get_pretrained('facebook/musicgen-melody')
+
+    model.set_generation_params(duration=8)  # generate 8 seconds.
+
     context = {
         "model": model
     }
@@ -22,12 +28,35 @@ def init():
 def handler(context: dict, request: Request) -> Response:
     prompt = request.json.get("prompt")
     model = context.get("model")
-    outputs = model(prompt)
+    descriptions = [prompt]
+
+    wav = model.generate(descriptions)[0].cpu().numpy()
+
+    ffmpeg_cmd = [
+        "ffmpeg",
+        "-f", "f32le",
+        "-ar", str(model.sample_rate),
+        "-ac", "1",
+        "-i", "-",
+        "-ar", "44100",
+        "-ac", "2",
+        "-f", "wav",
+        "-",
+    ]
+
+    process = subprocess.Popen(
+        ffmpeg_cmd, stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE
+    )
+
+    wav = wav.astype("float32").tobytes()
+    stdout, stderr = process.communicate(wav)
 
     return Response(
-        json = {"outputs": outputs[0]}, 
+        body=stdout,
+        headers={"Content-Type": "audio/wav"},
         status=200
     )
+
 
 if __name__ == "__main__":
     app.serve()
